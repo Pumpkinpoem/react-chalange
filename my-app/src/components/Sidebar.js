723dd893a93f28536aff9a231840821c23f42064
Andreas Almen <andreas.almen@live.com>
2024-07-31 11:10:00 +0000

    NavBarSimple

--- my-app/src/components/Sidebar.js
+++ my-app/src/components/Sidebar.js
@@ -1,8 +1,9 @@
 import React from "react";
+import css from './css/Sidebar.module.css'
 
 const Sidebar = () => {
     console.log();
-    return <div>
+    return <div class= {css.sidebar}>
         <a>My Photos</a>
         <a>My Illustrations</a>
         <a>My Paintings</a>
